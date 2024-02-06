01199f427642575173f83dbd6903ac6d8797f22f
Yezda <145451081+yezdanuracik@users.noreply.github.com>
2024-02-06 11:12:56 +0300

    Add files via upload

---
+++ stack 815/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace stack_815
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnMusteri = new System.Windows.Forms.Button();
+            this.btnSiraAl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAd.Location = new System.Drawing.Point(183, 116);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(46, 18);
+            this.lblAd.TabIndex = 7;
+            this.lblAd.Text = "label1";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(60, 54);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 20);
+            this.txtAd.TabIndex = 6;
+            // 
+            // btnMusteri
+            // 
+            this.btnMusteri.Location = new System.Drawing.Point(186, 54);
+            this.btnMusteri.Name = "btnMusteri";
+            this.btnMusteri.Size = new System.Drawing.Size(100, 38);
+            this.btnMusteri.TabIndex = 5;
+            this.btnMusteri.Text = "Müşteri çağır";
+            this.btnMusteri.UseVisualStyleBackColor = true;
+            this.btnMusteri.Click += new System.EventHandler(this.btnMusteri_Click);
+            // 
+            // btnSiraAl
+            // 
+            this.btnSiraAl.Location = new System.Drawing.Point(60, 105);
+            this.btnSiraAl.Name = "btnSiraAl";
+            this.btnSiraAl.Size = new System.Drawing.Size(100, 35);
+            this.btnSiraAl.TabIndex = 4;
+            this.btnSiraAl.Text = "Sıra al";
+            this.btnSiraAl.UseVisualStyleBackColor = true;
+            this.btnSiraAl.Click += new System.EventHandler(this.btnSiraAl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 191);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.btnMusteri);
+            this.Controls.Add(this.btnSiraAl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnMusteri;
+        private System.Windows.Forms.Button btnSiraAl;
+    }
+}
+
---
+++ stack 815/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stack_815
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> musteri = new Stack<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSiraAl_Click(object sender, EventArgs e)
+        {
+            musteri.Push(txtAd.Text);
+
+        }
+
+        private void btnMusteri_Click(object sender, EventArgs e)
+        {
+            if (musteri.Count > 0)
+            {
+                lblAd.Text = musteri.Pop();
+            }
+            else
+            {
+                MessageBox.Show("Müsteri yok.");
+            }
+        }
+    }
+}
